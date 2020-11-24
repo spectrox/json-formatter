c2ec42f28bf391fc63c5516c2aa7656fc098a4e1
spectrox <spectrox25@gmail.com>
2020-11-24 22:09:06 +0300

    Join split string and print formatted after merge

--- main.go
+++ main.go
@@ -5,20 +5,27 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+
+	"github.com/tidwall/pretty"
 )
-import "github.com/tidwall/pretty"
+
+const StringSplitLength = 8192
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	buffer := ""
 
 	for scanner.Scan() {
-		text := scanner.Text()
+		text := buffer + scanner.Text()
+		buffer = ""
 
 		if json.Valid([]byte(text)) {
 			formatted := pretty.Pretty([]byte(text))
 			result := pretty.Color(formatted, nil)
 
 			fmt.Println(string(result))
+		} else if isSplitJsonString(text) {
+			buffer = text
 		} else {
 			fmt.Println(text)
 		}
@@ -27,4 +34,20 @@ func main() {
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
 	}
+}
+
+func isSplitJsonString(text string) bool {
+	//if len(text) != StringSplitLength {
+	//	return false
+	//}
+
+	if string(text[0]) != "{" {
+		return false
+	}
+
+	if string(text[len(text) - 1]) == "}" {
+		return false
+	}
+
+	return true
 }
